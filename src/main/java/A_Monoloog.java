f766b7008e8f81dfbe3df76d20467ed6b1340210
mjerta <maarten.postma1000@gmail.com>
2024-05-11 22:31:47 +0200

    in progress

--- src/main/java/A_Monoloog.java
+++ src/main/java/A_Monoloog.java
@@ -3,14 +3,22 @@ public class A_Monoloog {
     public static void main(String[] args) {
         // Groepeer de statement in hun eigen method, en roep de method aan vanuit main. De code blijft functioneel hetzelfde doen.
         // Een conversatie bestaat uit een `greeting`, wat `smallTalk` en een `goodbye`
+        greeting();
+        smallTalk();
+        goodbye();
+    }
+
+    // nieuwe methods komen hier
+    private static void greeting() {
         System.out.println("Hey");
+    }
+    private static void smallTalk() {
         System.out.println("Welcome to this wonderful conversation");
         System.out.println("How about this weather?");
         System.out.println("And did you see that random program on that random channel?");
         System.out.println("Thank you for a wonderful conversation!");
+    }
+    private static void goodbye() {
         System.out.println("Cya");
     }
-
-    // nieuwe methods komen hier
-
 }
--- src/main/java/C_Oppervlakte.java
+++ src/main/java/C_Oppervlakte.java
@@ -4,19 +4,32 @@ public class C_Oppervlakte {
         int antwoord = 0;
         // Uncomment de regels en maak een method die voor het juiste antwoord zorgt.
 
-        //antwoord = oppervlakteRechthoek(2, 8);
+        antwoord = oppervlakteRechthoek(2, 8);
         System.out.println("De oppervlakte van een rechthoek met lengte 2 en breedte 8 is " + antwoord);
 
-        //antwoord = oppervlakteVierkant(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteVierkant(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
         System.out.println("De oppervlakte van een vierkant met zijden van 5 cm is " + antwoord);
 
-        //antwoord = oppervlakteDoos(2,8,5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
+        antwoord = oppervlakteDoos(2,8,5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteRechthoek()` hergebruiken.
         System.out.println("De oppervlakte van een doos met lengte 2, breedte 8 en hoogte 5 is " + antwoord);
 
-        //antwoord = oppervlakteKubus(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteVierkant()` hergebruiken, of `oppervlakteDoos()` hergebruiken.
+        antwoord = oppervlakteKubus(5); // Je kunt bij de implementatie van deze method een nieuwe berekening doen, of `oppervlakteVierkant()` hergebruiken, of `oppervlakteDoos()` hergebruiken.
         System.out.println("De oppervlakte van een kubus met zijden van 5 cm is " + antwoord);
     }
 
     // nieuwe methods komen hier
+    public static int oppervlakteRechthoek(int length, int width) {
+        return length * width;
+    }
+    public static int oppervlakteVierkant(int size) {
+        return oppervlakteRechthoek(size,size);
+    }
+    public static int oppervlakteDoos(int length, int width, int size) {
+        return oppervlakteRechthoek(length, width) * size;
+    }
+    public static int oppervlakteKubus(int size) {
+        return oppervlakteVierkant(size) * 6;
+    }
+
 
 }
